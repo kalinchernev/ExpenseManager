cf6571db000794b64a5381a8e127685b7779e042
Kalin Chernev <kalin.chernev@gmail.com>
2013-09-18 10:19:23 +0300

    simplifications

--- assets/js/model.js
+++ assets/js/model.js
@@ -24,8 +24,6 @@ document.addEventListener("DOMContentLoaded", function(){
         }
  
         openRequest.onsuccess = function(e) {
-            // console.log("Success!");
-            // Assuming db is a database variable opened successfully earlier
             db = e.target.result;
             
             if (document.querySelector("#save-button")){
@@ -77,12 +75,11 @@ function addExpense(e){
     }
  
     request.onsuccess = function(e) {
-        // console.log("Expense has been added");
         location.reload();
     }
 }
 
-// Helper to generate a table with the expenses
+// Displaying markup of table rows and data cells with data from the storage
 function displayExpensesOverview(e) {
 
     var expansesTable = "";
@@ -132,9 +129,8 @@ function backupExpenses(e) {
     }
 }
 
-/* Helper to convert JSON object to CSV file and dump it from the browser
+/* Helper dump json object as a CSV file
 * @param object JSON object to be converted to CSV file
-* @return browser dump of a CSV file containing the JSON object data re-ordered in a proper way
 */
 function JSON2CSV(expensesDump){
     var expenseID = new Array();
@@ -142,25 +138,12 @@ function JSON2CSV(expensesDump){
         expenseCurrency = new Array();
         expenseCategory = new Array();
         expenseDate = new Array();
-    var CSV = new Array();
-    var header = "expenseID,expenseValue,expenseCurrency,expenseCategory,expenseDate" + "\n";
+    var CSV = "expenseID,expenseValue,expenseCurrency,expenseCategory,expenseDate" + "\n";
     for (var i = 0; i < expensesDump.length; i++) {
         var expenseJSON = JSON.parse(expensesDump[i]);
         var item = expenseJSON.expenseID + "," + expenseJSON.expenseValue + "," + expenseJSON.expenseCurrency + "," + expenseJSON.expenseCategory + "," + expenseJSON.expenseDate;
         item = item + "\n";
-        CSV.push(item);
+        CSV += item;
     }
-    CSV = array2string(CSV);
-    // console.log(CSV);
-    file = header + CSV;
-    window.open("data:text/csv;charset=utf-8," + escape(file));
-}
-
-function array2string(array){
-    var array = array;
-    var string = "";
-    for (var i = 0; i < array.length; i++) {
-        string += array[i];
-    };
-    return string;
+    window.open("data:text/csv;charset=utf-8," + escape(CSV));
 }
